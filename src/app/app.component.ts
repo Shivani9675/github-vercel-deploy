960f8ef517b317b9af6a91cc7ddd025545f76a2a
Shivani9675 <prajapatishivuuu@gmail.com>
2024-12-20 15:39:10 +0530

    Update title for test job

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'github-vercel-deploy';
+  title = 'Github-Vercel-Deploy';
 }
